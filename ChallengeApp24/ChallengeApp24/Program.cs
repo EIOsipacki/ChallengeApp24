d4d14be074f083a08eb91cb7eac043a846e6c430
User <User@DESKTOP-ITK88ON>
2024-01-11 21:11:42 +0100

    HW Dzien 4

--- ChallengeApp24/ChallengeApp24/Program.cs
+++ ChallengeApp24/ChallengeApp24/Program.cs
@@ -1,2 +1,31 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+int age = 33;
+//Man, man, woman, Woman
+string gender = "Woman";
+
+
+if (gender == "Woman" || gender == "woman")
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        if (name == "Ewa" && age == 30)
+        {
+            Console.WriteLine("Ewa, lat 30");
+        }
+    }
+}
+else
+if (gender == "Man" || gender == "man")
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+    else
+    {
+    }
+}
